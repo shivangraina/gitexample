3abc4a449b3627bb084b6e5ea693dc9bfc82dd7d
Athmika Hebbar <athmikah@gmail.com>
2021-07-18 19:19:08 +0530

    Rename folder names to gitexample
    Removed naming conflicts in folders.

--- src/gitexample/core.clj
+++ src/gitexample/core.clj
@@ -1,51 +1,50 @@
-(ns gitexample.core)
-(defn count-word-frequency
-​
-  [word wordlist]
-  (count (filter #{word} wordlist)))
-​
-​
-(defn get-frequency-count
-​
-  [wordlist]
-  (into {}
-        (map #(vector % (count-word-frequency % wordlist))
-             (into #{} wordlist))))
-​
-​
-(defn first-n-fibonacci
+(require '[clojure.string :as str])
+(ns example-programs.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+;; Program 1
+
+(defn fizzbuzz
   [n]
-  (loop [final '[]
-         a  0
-         b  1
-         count 0]
-    (if (= count n)
-      final
-      (recur (conj final b)
-             b
-             (+ a b)
-             (inc count)))))
-​
-​
-(defn get-first-n-fibonacci
-  [n]
-  (apply list (first-n-fibonacci n)))
-​
-​
-(defn check-fizzbuzz
-  [n]
-  (if (and (zero? (rem n 3))
-           (zero? (rem n 5)))
-    "FIZZBUZZ"
-    (if (zero? (rem n 3))
-      "FIZZ"
-      (if (zero? (rem n 5))
+  (cond (= 0 (mod n 15))
+        "FIZZBUZZ"
+        (= 0 (mod n 3))
+        "FIZZ"
+        (= 0 (mod n 5))
         "BUZZ"
-        n))))
-​
-​
-(defn get-fizzbuzz
+        :else n))
+(defn generate-fizzbuzz
   [n]
-  (map #(check-fizzbuzz %) (map inc (range n))))
+  (vec (map fizzbuzz (range 1 (inc n)))))
+
+;;Program 2
+
+(defn fibo
+  "This function takes three values: `n`, `x` and `y` and generates the `n`th fibonacci number . This is a helper function for `fibo-seq`."
+  [n x y]
+  (if (< n 2) x (fibo (- n 1) y (+ x y))))
+
+(defn fibo-seq
+  "This function generates the fibonacci sequence for the first n numbers."
+  [n]
+  (map #(fibo % 1 1) (range 1 (+ n 1))))
+
+
+;; Program 3: finding frequency of each word in a string
+(defn split
+  [string]
+  (str/split string #" "))
+
+(defn count-freq
+  [string word]
+  (count (filter #{word} (split string))))
+
+(defn count-words
+  [string]
+  (zipmap (distinct (split string)) (map #(count-freq string %) (distinct (split string)))))
 
 
