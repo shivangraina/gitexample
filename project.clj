16d6903b4d8a2970f8c800d0b3af65fb6cf732ae
Shivang Raina <shivang@helpshift.com>
2021-07-19 14:32:16 +0530

    Fix merging changes in project.clj

--- project.clj
+++ project.clj
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-(defproject example-programs "0.1.0-SNAPSHOT"
-=======
 (defproject gitexample "0.1.0-SNAPSHOT"
->>>>>>> shivang/feature/fix-folder-structure
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
-<<<<<<< HEAD
-  :repl-options {:init-ns example-programs.core})
-=======
   :repl-options {:init-ns gitexample.core})
->>>>>>> shivang/feature/fix-folder-structure
